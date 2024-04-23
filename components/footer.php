5c17807cc62f5db8dc938c823ddbe33f10aa4d01
17Cheikhou <152703411+17Cheikhou@users.noreply.github.com>
2024-04-23 23:48:00 +0000

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,43 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Email</h3>
+         <a href="mailto:kevinmbaye17@gmail.com">kevinmbaye17@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Heure Ouvert</h3>
+         <p>08h 00 a 02h 00</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3> Addresse</h3>
+         <a href="#">Senegal</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Numeros </h3>
+         <a href="tel:1234567890">+221 771234567</a>
+         <a href="tel:1112223333">+221 771234567</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Pape Cheikh</span> | all rights reserved!
+            <a href="#" class="fab fa-facebook"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-youtube"></a>
+            <a href="#" class="fab fa-twitter"></a>
+</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
